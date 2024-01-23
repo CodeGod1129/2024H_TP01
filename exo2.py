bbae83fffe789184e506da6edfaf66f1cdf8edd9
CodeGod1129 <144284539+CodeGod1129@users.noreply.github.com>
2024-01-23 10:57:05 -0500

    1st

--- exo2.py
+++ exo2.py
@@ -1,3 +1,9 @@
 # TODO : Calculer les quantités de matériaux nécessaires pour fabriquer un nombre donné de baguettes magiques.
 
-nombre_baguettes = ...
+nombre_baguettes =input("Nombre de baguettes a fabriquer :")
+bois=int(nombre_baguettes)* 3
+ml=int(nombre_baguettes)* 10
+print(f" Voici les materiaux requis pour la fabrication de {nombre_baguettes} baguettes magiques:")
+print(f"- {bois} piece(s) de bois")
+print(f"- {nombre_baguettes} coeur(s) de creatures magiques")
+print(f"- {ml} ml de vernis")
